00c25228f844acd9e3de8cd9240fe6c8b4a8414d
roushanku <roushanku@iitbhilai.ac.in>
2025-10-29 00:06:16 +0530

    FIX : issue in getting submission status by prob service:

--- problem/src/main/java/com/codear/problem/SubmissionService.java
+++ problem/src/main/java/com/codear/problem/SubmissionService.java
@@ -1,23 +1,73 @@
 package com.codear.problem;
 
 import com.codear.problem.dto.Submission;
+import com.codear.problem.enums.SubmissionStatus;
 import com.codear.problem.repository.SubmissionRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mapping.AccessOptions.SetOptions.Propagation;
+
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class SubmissionService {
-    @Autowired
-    private SubmissionRepository submissionRepository;
+    private final SubmissionReadService submissionReadService;
 
-    public Submission getSubmissionById(String submissionId) {
-        return submissionRepository.findBySubmissionId(submissionId)
-                .orElseThrow(() -> new RuntimeException("Submission not found"));
+    // @Transactional(propagation = Propagation.REQUIRES_NEW, readOnly = true)
+    // public Submission getSubmissionById(String submissionId, int waited) {
+    //     System.out.println("Fetching submission using Id: " + submissionId + " waited=" + waited);
+
+    //     return submissionRepository.findBySubmissionId(submissionId)
+    //             .orElseThrow(() -> new RuntimeException("Submission not found"));
+    // }
+
+
+    public Submission longPollingService(String submissionId) throws InterruptedException {
+        int maxWaitTime = 10000;  // 10s
+        int baseInterval = 2000;
+        int waited = 0;
+
+        System.out.println("Entered long polling service for " + submissionId);
+
+        SubmissionStatus submission = submissionReadService.getSubmissionStatusById(submissionId, waited);
+
+        long startTime = System.currentTimeMillis();
+        System.out.println("⏳ Entered polling loop for " + submissionId);
+
+        try {
+            while (submission == SubmissionStatus.IN_PROGRESS && waited < maxWaitTime) {
+                // int currentInterval = Math.min(baseInterval * (1 << (waited / baseInterval)), 2000);
+                // int currentInterval = 100;
+                Thread.sleep(baseInterval);
+                waited += baseInterval;
+
+                submission = submissionReadService.getSubmissionStatusById(submissionId , waited);
+                System.out.println("Looping... waited=" + waited + " status=" + submission);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("❌ Exception in polling loop: " + e.getMessage());
+        }
+
+        long duration = System.currentTimeMillis() - startTime;
+        System.out.println("✅ Exited polling loop for " + submissionId);
+        System.out.println("⏱️ Total time spent: " + duration + " ms");
+        System.out.println("Final submission status: " + submission);
+
+        Submission submission2 = submissionReadService.getSubmissionById(submissionId, waited);
+        submission2.setStatus(submission);
+        return submission2;
     }
 
-    public List<Submission> getSubmissionsByProblemId(Long problemId) {
-        return submissionRepository.findByProblemId(problemId);
-    }
+
+
+    // public List<Submission> getSubmissionsByProblemId(Long problemId) {
+    //     return submissionRepository.findByProblemId(problemId);
+    // }
 
 }
--- problem/src/main/java/com/codear/problem/controller/SubmissionController.java
+++ problem/src/main/java/com/codear/problem/controller/SubmissionController.java
@@ -2,7 +2,6 @@ package com.codear.problem.controller;
 
 import com.codear.problem.SubmissionService;
 import com.codear.problem.dto.Submission;
-import com.codear.problem.enums.SubmissionStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
@@ -16,31 +15,12 @@ public class SubmissionController {
 
     @GetMapping("/{submissionId}")
     public Submission longPollStatus(@PathVariable String submissionId) throws InterruptedException {
-        int maxWaitTime = 15000;  // 15 seconds
-        int baseInterval = 100;  // 100ms
-        int waited = 0;
-        int ct = 0;
-
-        Submission submission = submissionService.getSubmissionById(submissionId);
-
-        while (submission.getStatus() == SubmissionStatus.IN_PROGRESS && waited < maxWaitTime) {
-            int currentInterval = Math.min(baseInterval * (1 << (ct)), 2000); 
-            // ensure interval caps at 2000ms to avoid too long waits
-
-            Thread.sleep(currentInterval);
-            waited += currentInterval;
-
-            submission = submissionService.getSubmissionById(submissionId);
-            ct++;
-        }
-
-        System.out.println("submission" + submission);
-
-        return submission;
+        System.out.println("Entreted submission controller");
+        return submissionService.longPollingService(submissionId);
     }
 
-    @GetMapping("/all")
-    public List<Submission> getSubmissionsByProblemId(@RequestParam Long problemId) {
-        return submissionService.getSubmissionsByProblemId(problemId);
-    }
+    // @GetMapping("/all")
+    // public List<Submission> getSubmissionsByProblemId(@RequestParam Long problemId) {
+    //     return submissionService.getSubmissionsByProblemId(problemId);
+    // }
 }
